10061a4f9c89b53680be653c4e09e34e9274b55b
BenCurran0 <bmcmorrancurran@gmail.com>
2025-03-06 17:14:37 +0000

    underConstruction

--- Server_23369205.java
+++ Server_23369205.java
@@ -1,12 +1,13 @@
-
 package com.mycompany.tcpechoclient;
 
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 public class Server_23369205 {
     
     private static final int port = 1234;
+    private static HashMap<String, String> courseSchedule = new HashMap<>(); 
 
     public static void main(String[] args) {
         ServerSocket socket = null;
@@ -21,14 +22,14 @@ public class Server_23369205 {
                 
                 BufferedReader in;
                 PrintWriter out;
-                String clientMessage;
+                String clientRequest;
                 
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
                 
-                while ((clientMessage = in.readLine()) != null) {
-                    System.out.println("Message from the client: " + clientMessage);
-                    out.println(clientMessage);
+                while ((clientRequest = in.readLine()) != null) {
+                    String response = checkRequest(clientRequest);
+                    out.println(response);
                 }
 
                 clientSocket.close();
@@ -49,5 +50,49 @@ public class Server_23369205 {
                 System.out.println("Unable to disconnect/close!");
             }
         }
+    }
+
+
+    private static String checkRequest(String request) {
+
+        String[] parts = request.split(" ");
+
+            String command = parts[0].toUpperCase();
+            String moduleCode = parts[1];
+
+            switch (command) {
+                case "ADD":
+                if (parts.length < 4) {
+                    return "Invalid ADD format";
+                }
+                return addLecture(moduleCode, parts[2], parts[3]);
+
+                case "REMOVE":
+                    removeLecture(moduleCode);
+                        return "Lecture removed: " + moduleCode;
+                    
+                case "DISPLAY":
+                    displaySchedule();
+                    
+                default:
+                    return "Unknown command";
+            
+        }
+    }
+
+    private static String addLecture(String moduleCode, String room, String time) {
+        int[] timeSplit  = time.split(-);
+        int start = timeSplit[0];
+        int end = timeSplit[1];
+        courseSchedule.put();
+        return "Lecture added: " + moduleCode + " " + room + "  " + time;
+    }
+
+    private static String removeLecture(String moduleCode){
+
+    }
+
+    private static String displaySchedule() {
+
     }
 }
