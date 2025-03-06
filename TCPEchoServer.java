903fb2bbf63382b3362b2abe767f1f734778b14d
BenCurran0 <bmcmorrancurran@gmail.com>
2025-03-06 15:48:38 +0000

    p

--- TCPEchoServer.java
+++ TCPEchoServer.java
@@ -1,69 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.tcpechoserver;
+
+package com.mycompany.tcpechoclient;
+
 import java.io.*;
 import java.net.*;
 
-/**
- *
- * @author razi
- */
-public class TCPEchoServer { 
-  private static ServerSocket servSock;
-  private static final int PORT = 1234;
-  private static int clientConnections = 0;
-
-  public static void main(String[] args) {
-    System.out.println("Opening port...\n");
-    try 
-    {
-        servSock = new ServerSocket(PORT);      //Step 1.
-    }
-    catch(IOException e) 
-    {
-         System.out.println("Unable to attach to port!");
-         System.exit(1);
-    }
+public class TCPEchoServer {
     
-    do 
-    {
-         run();
-    }while (true);
+    private static final int port = 1234;
 
-  }
-  
-  private static void run()
-  {
-    Socket link = null;                        //Step 2.
-    try 
-    {
-      link = servSock.accept();               //Step 2.
-      clientConnections++;
-      BufferedReader in = new BufferedReader( new InputStreamReader(link.getInputStream())); //Step 3.
-      PrintWriter out = new PrintWriter(link.getOutputStream(),true); //Step 3.
-      
-      String message = in.readLine();         //Step 4.
-      System.out.println("Message received from client: " + clientConnections + "  "+ message);
-      out.println("Response from Server (Capitalized Message): " + message.toUpperCase());     //Step 4.
-     }
-    catch(IOException e)
-    {
-        e.printStackTrace();
+    public static void main(String[] args) {
+        ServerSocket socket = null;
+        
+        try {
+            socket = new ServerSocket(port);
+            System.out.println("Server is waiting for connection from client");
+
+            
+                Socket clientSocket = socket.accept();
+                System.out.println("Client connected from " + clientSocket.getInetAddress());
+                
+                BufferedReader in;
+                PrintWriter out;
+                String clientMessage;
+                
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                
+                while ((clientMessage = in.readLine()) != null) {
+                    System.out.println("Message from the client: " + clientMessage);
+                    out.println(clientMessage);
+                }
+
+                clientSocket.close();
+                System.out.println("Connection with client is now closed");
+            
+        } 
+        catch (IOException e) {
+            System.out.println("IOException occurred: " + e.getMessage());
+        }
+        
+        finally {
+            try {
+                if (socket != null  && !socket.isClosed()) {
+                    socket.close();
+                    System.out.println("Server socket is now closed.");
+                }
+            } catch (IOException e) {
+                System.out.println("Unable to disconnect/close!");
+            }
+        }
     }
-    finally 
-    {
-       try {
-	    System.out.println("\n* Closing connection... *");
-            link.close();				    //Step 5.
-	}
-       catch(IOException e)
-       {
-            System.out.println("Unable to disconnect!");
-	    System.exit(1);
-       }
-    }
-  } // finish run method 
-} // finish the class
+}
